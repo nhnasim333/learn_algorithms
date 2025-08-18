5c567fd3d8670995b03b79450c78f2d7d053753d
Nasim <mdnasimhosen333@gmail.com>
2025-08-18 11:50:03 +0600

    fix: update output of adjacency matrix to reflect correct connections

--- Graph Representation/adjacency_matrix_of_undirected_graph.cpp
+++ Graph Representation/adjacency_matrix_of_undirected_graph.cpp
@@ -21,5 +21,13 @@ int main()
         adj_mat[a][b] = 1;
         adj_mat[b][a] = 1;
     }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << adj_mat[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
