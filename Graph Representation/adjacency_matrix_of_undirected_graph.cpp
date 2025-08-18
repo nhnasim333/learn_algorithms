0a85d0fdd34292c216a82db875d5fc923c41592a
Nasim <mdnasimhosen333@gmail.com>
2025-08-18 11:58:06 +0600

    refactor: remove commented initialization loop for adjacency matrix

--- Graph Representation/adjacency_matrix_of_undirected_graph.cpp
+++ Graph Representation/adjacency_matrix_of_undirected_graph.cpp
@@ -6,14 +6,19 @@ int main()
     int n, e;
     cin >> n >> e;
     int adj_mat[n][n];
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 0; j < n; j++)
-    //     {
-    //         adj_mat[i][j] = 0;
-    //     }
-    // }
     memset(adj_mat, 0, sizeof(adj_mat));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                adj_mat[i][j] = 1;
+            }
+        }
+    }
+    
     while (e--)
     {
         int a, b;
