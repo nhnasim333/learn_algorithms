ff3461ed9c35e5fdbd5c14d515d4777e0c23fcfe
Nasim <mdnasimhosen333@gmail.com>
2025-08-18 11:43:18 +0600

    add adjacency matrix implementation for undirected graph

---
+++ Graph Representation/adjacency_matrix_of_undirected_graph.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int adj_mat[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            adj_mat[i][j] = 0;
+        }
+    }
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+    }
+    return 0;
+}
