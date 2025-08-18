c5f8538f49576c36f3456a04bd5000309bc7533d
Nasim <mdnasimhosen333@gmail.com>
2025-08-18 11:45:03 +0600

    Refactor code structure for improved readability and maintainability

--- Graph Representation/adjacency_matrix_of_undirected_graph.cpp
+++ Graph Representation/adjacency_matrix_of_undirected_graph.cpp
@@ -6,13 +6,15 @@ int main()
     int n, e;
     cin >> n >> e;
     int adj_mat[n][n];
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            adj_mat[i][j] = 0;
-        }
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         adj_mat[i][j] = 0;
+    //     }
+    // }
+    memset(adj_mat, 0, sizeof(adj_mat));
+
     while (e--)
     {
         int a, b;
