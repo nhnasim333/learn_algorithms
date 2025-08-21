94ec067a84875e4d5df640ed16d9f3e94bee6155
Nasim <mdnasimhosen333@gmail.com>
2025-08-21 16:55:43 +0600

    Refactor DFS implementation for improved correctness and add validation checks

--- Graph Representation/dfs_on_2d_grid.cpp
+++ Graph Representation/dfs_on_2d_grid.cpp
@@ -3,6 +3,14 @@ using namespace std;
 char grid[105][105];
 bool vis[105][105];
 vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int n, m;
+
+bool valid(int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m)
+        return false;
+    return true;
+}
 
 void dfs(int si, int sj)
 {
@@ -10,17 +18,16 @@ void dfs(int si, int sj)
     vis[si][sj] = true;
     for (int i = 0; i < 4; i++)
     {
-        int ci, cj;
-        ci = si + directions[i].first;
-        cj = sj + directions[i].second;
+        int ci = si + directions[i].first;
+        int cj = sj + directions[i].second;
 
-        cout << ci << " " << cj << endl;
+        if (!vis[ci][cj] && valid(ci, cj))
+            dfs(ci, cj);
     }
 }
 
 int main()
 {
-    int n, m;
     cin >> n >> m;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
@@ -28,6 +35,7 @@ int main()
 
     int si, sj;
     cin >> si >> sj;
+    memset(vis, false, sizeof(vis));
 
     dfs(si, sj);
 
