d196984b521e08fd1fc3b98ed569de585b63f9f0
Nasim <mdnasimhosen333@gmail.com>
2025-08-23 12:15:21 +0600

    Refactor DFS implementation to correctly count connected components and update input/output files

--- Graph Representation/p2_d2.cpp
+++ Graph Representation/p2_d2.cpp
@@ -3,14 +3,12 @@ using namespace std;
 vector<int> adj_list[1005];
 bool vis[1005];
 
-int dfs(int src)
+void dfs(int src)
 {
     vis[src] = true;
-    int cnt = 1;
     for (int child : adj_list[src])
         if (!vis[child])
-            cnt += dfs(child);
-    return cnt;
+            dfs(child);
 }
 
 int main()
@@ -25,17 +23,15 @@ int main()
         adj_list[b].push_back(a);
     }
     memset(vis, false, sizeof(vis));
-    vector<int> components_sizes;
+    int cnt = 0;
     for (int i = 0; i < n; i++)
     {
         if (!vis[i])
         {
-            int size = dfs(i);
-            components_sizes.push_back(size);
+            dfs(i);
+            cnt++;
         }
     }
-    for (int comp : components_sizes)
-        cout << comp << " ";
-    cout << endl;
+    cout << cnt << endl;
     return 0;
 }
