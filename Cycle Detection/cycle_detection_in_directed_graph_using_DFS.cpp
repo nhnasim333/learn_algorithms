3cfa3fad288a38de10acf2b0b1291bf7f2363251
Nasim <mdnasimhosen333@gmail.com>
2025-08-26 10:59:35 +0600

    Refactor code structure for improved readability and maintainability

---
+++ Cycle Detection/cycle_detection_in_directed_graph_using_DFS.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adj_list[105];
+int vis[105];
+int pathvis[105];
+bool cycle;
+
+void dfs(int src)
+{
+    vis[src] = true;
+    pathvis[src] = true;
+
+    for (int child : adj_list[src])
+    {
+        if (vis[child] && pathvis[src])
+            cycle = true;
+        if (!vis[child])
+        {
+            dfs(child);
+        }
+    }
+    pathvis[src] = false;
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    memset(vis, false, sizeof(vis));
+    memset(pathvis, false, sizeof(pathvis));
+    cycle = false;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            dfs(i);
+        }
+    }
+    if (cycle)
+        cout << "Cycle detected" << endl;
+    else
+        cout << "No cycle" << endl;
+
+    return 0;
+}
