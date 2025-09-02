7c12746beb2e526fdbb41bc9f1260eb14eb5f8a2
Nasim <mdnasimhosen333@gmail.com>
2025-09-02 10:57:50 +0600

    Implement Bellman-Ford algorithm with cycle detection

---
+++ Bellman Ford/bellman_ford_cycle_ditection.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Edge
+{
+public:
+    int a, b, w;
+    Edge(int a, int b, int w)
+    {
+        this->a = a;
+        this->b = b;
+        this->w = w;
+    }
+};
+
+int dis[105];
+vector<Edge> edge_list;
+int n, e;
+
+void bellman_ford()
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (auto ed : edge_list)
+        {
+            int a = ed.a;
+            int b = ed.b;
+            int w = ed.w;
+
+            if (dis[a] != INT_MAX && dis[a] + w < dis[b])
+                dis[b] = dis[a] + w;
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b, w;
+        cin >> a >> b >> w;
+        edge_list.push_back(Edge(a, b, w));
+    }
+
+    for (int i = 0; i < n; i++)
+        dis[i] = INT_MAX;
+
+    dis[0] = 0;
+
+    bellman_ford();
+
+    for (int i = 0; i < n; i++)
+        cout << i << "->" << dis[i] << endl;
+
+    return 0;
+}
