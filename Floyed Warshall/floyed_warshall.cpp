82cc2d9867f854083fb59850880bae7a0068f8cc
Nasim <mdnasimhosen333@gmail.com>
2025-09-03 14:16:27 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ Floyed Warshall/floyed_warshall.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int adj_mat[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            // if (i == j)
+            //     adj_mat[i][j] = 0;
+            // else
+            adj_mat[i][j] = INT_MAX;
+        }
+    }
+
+    while (e--)
+    {
+        int a, b, w;
+        cin >> a >> b >> w;
+        adj_mat[a][b] = w;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (adj_mat[i][j] == INT_MAX)
+                cout << "INF" << " ";
+            else
+                cout << adj_mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
