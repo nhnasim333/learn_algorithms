73ea161a367e3e45271e2b71e95384eaa648209b
Nasim <mdnasimhosen333@gmail.com>
2025-09-04 21:51:24 +0600

    Update DSU output and modify union logic for improved functionality

--- Disjoint Set Union DSU/union.cpp
+++ Disjoint Set Union DSU/union.cpp
@@ -18,7 +18,7 @@ void dsu_union(int node1, int node2)
     int leader1 = find(node1);
     int leader2 = find(node2);
 
-    if (group_size[leader1] > group_size[leader2])
+    if (group_size[leader1] >= group_size[leader2])
     {
         par[leader2] = leader1;
         group_size[leader1] += group_size[leader2];
@@ -36,7 +36,8 @@ int main()
     memset(group_size, 1, sizeof(group_size));
 
     dsu_union(1, 2);
-    dsu_union(3, 5);
+    dsu_union(2, 0);
+    dsu_union(3, 1);
 
     cout << find(4) << endl;
 
