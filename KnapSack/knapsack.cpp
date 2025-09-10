f2450fc393d4cf7ebcbeb534a625dd6463087577
Nasim <mdnasimhosen333@gmail.com>
2025-09-10 19:35:19 +0600

    Refactor knapsack function to improve memoization and update dynamic programming table initialization

--- KnapSack/knapsack.cpp
+++ KnapSack/knapsack.cpp
@@ -1,20 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 int val[1005], weight[1005];
+int dp[1005][1005];
 
 int knapsack(int i, int mx_weight)
 {
     if (i < 0 || mx_weight <= 0)
         return 0;
+
+    if (dp[i][mx_weight] != -1)
+        return dp[i][mx_weight];
+
     if (weight[i] <= mx_weight)
     {
         int op1 = knapsack(i - 1, mx_weight - weight[i]) + val[i];
         int op2 = knapsack(i - 1, mx_weight);
-        return max(op1, op2);
+        dp[i][mx_weight] = max(op1, op2);
+        return dp[i][mx_weight];
     }
     else
     {
-        return knapsack(i - 1, mx_weight);
+        dp[i][mx_weight] = knapsack(i - 1, mx_weight);
+        return dp[i][mx_weight];
     }
 }
 
@@ -28,6 +35,9 @@ int main()
         cin >> weight[i];
     cin >> maxWeight;
 
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= maxWeight; j++)
+            dp[i][j] = -1;
     int ans = knapsack(n - 1, maxWeight);
     cout << ans << endl;
 
