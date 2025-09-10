8a10173f37babf83b5aa6afab6e7d75024784311
Nasim <mdnasimhosen333@gmail.com>
2025-09-10 19:31:21 +0600

    Implement code changes to enhance functionality and improve performance

--- KnapSack/knapsack.cpp
+++ KnapSack/knapsack.cpp
@@ -1,7 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+int val[1005], weight[1005];
 
-int main(){
-    
+int knapsack(int i, int mx_weight)
+{
+    if (i < 0 || mx_weight <= 0)
+        return 0;
+    if (weight[i] <= mx_weight)
+    {
+        int op1 = knapsack(i - 1, mx_weight - weight[i]) + val[i];
+        int op2 = knapsack(i - 1, mx_weight);
+        return max(op1, op2);
+    }
+    else
+    {
+        return knapsack(i - 1, mx_weight);
+    }
+}
+
+int main()
+{
+    int n, maxWeight;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> val[i];
+    for (int i = 0; i < n; i++)
+        cin >> weight[i];
+    cin >> maxWeight;
+
+    int ans = knapsack(n - 1, maxWeight);
+    cout << ans << endl;
+
     return 0;
 }
